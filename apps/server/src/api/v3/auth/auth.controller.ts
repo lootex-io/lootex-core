424e0462472bbdc92a4822db82d0d8f71b0d7c00
Simon Hsu <simon.hsu@lootex.io>
2025-12-17 23:07:39 +0800

    feat: Remove Campaign, Games, Auth related routes in routes.txt. Remove unused logic in AuthController and AuthService

--- apps/server/src/api/v3/auth/auth.controller.ts
+++ apps/server/src/api/v3/auth/auth.controller.ts
@@ -93,223 +93,21 @@ export class AuthController {
 
 
 
-  // @dev getChallenge: generates a random one-time challenge for the user
-  @Get('/challenge/get')
-  async getChallenge(
-    @Query() getChallengeDto: GetChallengeDto,
-  ): Promise<AuthGetChallengeResult> {
-    try {
-      if (
-        !WEB3_SUPPORTED_CHAIN_FAMILIES.includes(getChallengeDto.chainFamily)
-      ) {
-        throw new TypeError('getChallenge: Chain family not supported');
-      }
-      switch (getChallengeDto.chainFamily) {
-        case 'ETH':
-        case 'SOL':
-        case 'APTOS':
-        case 'FLOW':
-        case 'SUI':
-          return {
-            chainFamily: getChallengeDto.chainFamily,
-            address: getChallengeDto.address,
-            challenge: await this.authService.getOneTimeChallenge(
-              getChallengeDto.address,
-              getChallengeDto.chainFamily,
-            ),
-          } as AuthGetChallengeResult;
-        default:
-          throw new TypeError('getChallenge: Chain family not supported');
-      }
-    } catch (err) {
-      throw new HttpException(err.message, HttpStatus.BAD_REQUEST);
-    }
-  }
+
 
   // sendEmailOTP removed
 
   // isEmailAvailable and isUsernameAvailable removed
 
-  // @dev isWalletAddressAvailable: check if a wallet address has been registered with Lootex ID
-  @Get('/wallets/available')
-  async isWalletAddressAvailable(
-    @Query() isAddressAvailableDto: QueryByAddressBaseDto,
-  ): Promise<boolean> {
-    try {
-      const existingWalletCount = await this.walletsRepository.count({
-        where: {
-          address: isAddressAvailableDto.address,
-        },
-      });
-      return existingWalletCount === 0;
-    } catch (err) {
-      throw new HttpException(err.message, HttpStatus.UNAUTHORIZED);
-    }
-  }
 
-  // @dev signUp: Create Lootex ID
-  @Post('/web3/sign-up')
-  async signUp(
-    @Body() accountSignUpDto: AccountSignUpDto,
-    @FlexCookieOption() cookieOption: CookieSerializeOptions,
-    @Res({ passthrough: true }) response: ResponseWithCookie,
-    @RealIP() realIp: string,
-    @CFIpCountry() cfIpCountry: string,
-  ): Promise<AuthSignUpResult> {
-    try {
-      // IP check removed
 
-      if (
-        !WEB3_SUPPORTED_CHAIN_FAMILIES.includes(accountSignUpDto.chainFamily)
-      ) {
-        throw new TypeError('signUp: unrecognised chain family');
-      }
-      let newAccountWalletPair: [Account, Wallet] = null;
-      switch (accountSignUpDto.chainFamily) {
-        case 'ETH':
-          newAccountWalletPair = await this.authService.handleSignUpEth(
-            accountSignUpDto,
-            realIp,
-            cfIpCountry,
-          );
-          break;
-        default:
-          throw new TypeError('signUp: Chain family not supported');
-      }
-      const [newAccount, newWallet] = newAccountWalletPair;
-      const payload = new LootexJwtPayload(
-        newAccount.id,
-        newAccount.email,
-        newAccount.username,
-        newAccount.avatarUrl,
-        newWallet.id,
-      );
-      const jwtToken: string = await this.jwtService.signAsync(
-        payload.toObject(),
-        {
-          secret: this.configurationService.get('JWT_SECRET'),
-          algorithm: this.configurationService.get('JWT_ALGORITHM'),
-          expiresIn: this.configurationService.get('JWT_EXPIRES_IN'),
-        },
-      );
-      response.cookie(
-        this.configurationService.get('AUTH_JWT_COOKIE_KEY'),
-        jwtToken,
-        cookieOption,
-      );
 
-      return {
-        id: newAccount.id,
-        email: newAccount.email,
-        username: newAccount.username,
-        transport: newWallet.transport,
-        address: newWallet.address,
-        provider: newWallet.provider,
-        walletId: newWallet.id,
-      } as AuthSignUpResult;
-    } catch (err) {
-      throw new HttpException(err.message, HttpStatus.UNAUTHORIZED);
-    }
-  }
 
-  // @dev signIn: Sign in to Lootex ID
-  @UseInterceptors(AccountPrivateInterceptor)
-  @Post('/web3/sign-in')
-  async signIn(
-    @Body() accountSignInDto: AccountAuthBaseDto,
-    @FlexCookieOption() cookieOption: CookieSerializeOptions,
-    @Res({ passthrough: true }) response: ResponseWithCookie,
-    @RealIP() realIp: string,
-    @CFIpCountry() cfIpCountry: string,
-  ): Promise<any> {
-    try {
-      if (
-        !WEB3_SUPPORTED_CHAIN_FAMILIES.includes(accountSignInDto.chainFamily)
-      ) {
-        throw new TypeError('signIn: unrecognised chain family');
-      }
-      let jwtToken = '';
-      switch (accountSignInDto.chainFamily) {
-        case 'ETH':
-          jwtToken = await this.authService.handleSignInEth(accountSignInDto);
-          break;
-        default:
-          throw new TypeError(
-            `signIn: Chain family not supported: ${accountSignInDto.chainFamily}`,
-          );
-      }
-      response.cookie(
-        this.configurationService.get('AUTH_JWT_COOKIE_KEY'),
-        jwtToken,
-        cookieOption,
-      );
 
-      const jwdValue = this.jwtService.decode(jwtToken) as LootexJwtPayload;
 
-      this.accountService.updateAccountLastLogin({
-        username: jwdValue.username,
-        ip: realIp,
-        area: cfIpCountry,
-      });
 
-      return await this.accountService.getProfile(jwdValue.username);
-    } catch (err) {
-      if (
-        err instanceof HttpException &&
-        err.getStatus() === HttpStatus.FORBIDDEN
-      ) {
-        throw err;
-      } else {
-        throw new HttpException(err.message, HttpStatus.UNAUTHORIZED);
-      }
-    }
-  }
 
-  // @dev signout: Sign out from your Lootex ID, clears Cookie
-  @UseGuards(AuthJwtGuard)
-  @Get('/web3/sign-out')
-  async signOut(
-    @FlexCookieOption() cookieOption: CookieSerializeOptions,
-    @Res({ passthrough: true }) response: ResponseWithCookie,
-  ): Promise<boolean> {
-    try {
-      response.cookie(
-        this.configurationService.get('AUTH_JWT_COOKIE_KEY'),
-        '',
-        {
-          ...cookieOption,
-          expires: AUTH_COOKIE_EXPIRE_DATE,
-        },
-      );
-      return true;
-    } catch (err) {
-      throw new HttpException(err.message, HttpStatus.UNAUTHORIZED);
-    }
-  }
 
-  // @dev getAccountInfo: Retrieve my Lootex ID
-  @UseGuards(AuthJwtGuard)
-  @Get('/accounts/me')
-  async getAccountInfo(
-    @CurrentUser() user: Account,
-    @CurrentWallet() wallet: Wallet,
-  ): Promise<AuthGetAccountInfoResult> {
-    try {
-      const currentUser = user.toJSON();
-      const currentWallet = wallet.toJSON();
-      return {
-        id: currentUser.id,
-        email: currentUser.email,
-        username: currentUser.username,
-        fullname: currentUser.fullname,
-        avatarUrl: currentUser.avatarUrl,
-        introduction: currentUser.introduction,
-        walletId: currentWallet.id,
-      } as AuthGetAccountInfoResult;
-    } catch (err) {
-      throw new HttpException(err.message, HttpStatus.BAD_REQUEST);
-    }
-  }
 
   // isWalletBound removed
 
@@ -317,36 +115,7 @@ export class AuthController {
 
   // unbindWalletById removed
 
-  // @dev listWalletsByAccountId: retrieves array of bound wallets of one user
-  @UseGuards(AuthJwtGuard)
-  @Get('/accounts/:id/wallets')
-  async listWalletsByAccountId(
-    @CurrentUser() user: Account,
-    @Param('id') accountId: string,
-  ): Promise<Array<Wallet>> {
-    try {
-      // @dev ABAC later. Restrict access to own account for now.
-      if (accountId !== user.id)
-        throw new TypeError(
-          'listWalletsByAccountId: access for own account only',
-        );
-      return this.walletsRepository.findAll({
-        where: {
-          accountId: user.id,
-        },
-        attributes: [
-          'transport',
-          'provider',
-          'chainFamily',
-          'isMainWallet',
-          'address',
-          'status',
-        ],
-      });
-    } catch (err) {
-      throw new HttpException(err.message, HttpStatus.BAD_REQUEST);
-    }
-  }
+
 
   // @dev Awaiting implementation after ABAC
   // @Put('/web3/accounts/update')
